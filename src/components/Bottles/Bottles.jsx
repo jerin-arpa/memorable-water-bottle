d8d83be7175f577f2970ebc226a7c4f9d0e9cbb8
jerin-arpa <ismatjerrin3@gmail.com>
2023-09-12 05:16:52 +0600

    Display Saved cart from local storage

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Bottle from "../Bottle/Bottle";
 import './Bottles.css'
 import { addToLocalStorage, getStoredCart } from "../../utilities/localStorage";
+import Cart from "../Cart/Cart";
 
 
 const Bottles = () => {
@@ -24,8 +25,20 @@ const Bottles = () => {
         if (bottles.length > 0) {
             const storedCart = getStoredCart();
             console.log(storedCart);
+
+            const savedCart = [];
+            for (const id of storedCart) {
+                console.log(id);
+                const bottle = bottles.find(bottle => bottle.id === id);
+                if (bottle) {
+                    savedCart.push(bottle);
+                }
+            }
+            console.log('Saved Cart', savedCart);
+            setCart(savedCart);
         }
     }, [bottles]);
+
 
 
     const handleAddToCart = bottle => {
@@ -38,7 +51,9 @@ const Bottles = () => {
     return (
         <div>
             <h2>Bottles Available: {bottles.length}</h2>
-            <h4>Cart: {cart.length}</h4>
+
+            <Cart cart={cart}></Cart>
+
             <div className="bottle-container">
                 {
                     bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle} handleAddToCart={handleAddToCart}></Bottle>)
