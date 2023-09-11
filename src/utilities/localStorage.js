5d0606f53192661a07775d0643cca98056a28522
jerin-arpa <ismatjerrin3@gmail.com>
2023-09-12 04:41:20 +0600

    Store data just one time in local storage

---
+++ src/utilities/localStorage.js
@@ -0,0 +1,24 @@
+const getStoredCart = () => {
+    const storedCartString = localStorage.getItem('cart');
+    if (storedCartString) {
+        return JSON.parse(storedCartString);
+    }
+    return [];
+}
+
+
+const SaveCartToLocalStorage = cart => {
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringified);
+}
+
+
+const addToLocalStorage = id => {
+    const cart = getStoredCart();
+    cart.push(id);
+    // save to local storage
+    SaveCartToLocalStorage(cart);
+}
+
+
+export { addToLocalStorage };
