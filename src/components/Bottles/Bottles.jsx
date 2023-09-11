3126e7adad02f244f8baad4442c2064928ef75ec
jerin-arpa <ismatjerrin3@gmail.com>
2023-09-12 04:02:42 +0600

    first commit

---
+++ src/components/Bottles/Bottles.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Bottle from "../Bottle/Bottle";
+import './Bottles.css'
+
+
+const Bottles = () => {
+
+    const [bottles, setBottles] = useState(0);
+
+
+    useEffect(() => {
+        fetch('bottles.json')
+            .then(res => res.json())
+            .then(data => setBottles(data))
+    }, []);
+
+
+    return (
+        <div>
+            <h2>Bottles Here: {bottles.length}</h2>
+            <div className="bottle-container">
+                {
+                    bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}></Bottle>)
+                }
+            </div>
+        </div >
+    );
+};
+
+export default Bottles;
